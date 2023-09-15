b0d422557fb6d6761dc6d4aa92143d49fb039bab
zhangyue207 <zhangyue@qiyuanlab.com>
2023-09-15 11:48:56 +0800

    fix

--- include/core/micro.h
+++ include/core/micro.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "core/type.h"
-#include "core/utils.h"
 #include "core/cache.h"
 #include <map>
 #include <string>
@@ -9,9 +8,6 @@ namespace infini {
 
 // TODO: delete line after change to Kernel
 using MicroType = KernelType;
-
-// using MicroAttrs =
-//     std::tuple<PlatformType, MicroType, const int>;  // platfrom , type, ID
 
 class Micro {
   /**
@@ -35,59 +31,10 @@ class Micro {
    * function and call generations which belongs to
    * specific platform.
    */
-  virtual std::string generatorCode(Cache &cache,
-                                    std::string &result) = 0;
+  virtual std::string generatorCode(Cache &cache, std::string &result) = 0;
 
   /** @brief Information print*/
   virtual void printInformation();
 };
-
-// class MicroRegistry {
-//   /**
-//    * @brief MicroRistry is a single Instancing class
-//    * which contains different kinds of Micros
-//    */
-//  public:
-//   using MicroRecord =
-//       std::tuple<Micro *const, const std::string>;  // Micro* , name
-//  private:
-//   std::map<MicroAttrs, MicroRecord> micros;
-
-//  public:
-//   // Deconstruct
-//   ~MicroRegistry() {
-//     for (auto &[k, v] : micros) {
-//       delete std::get<0>(v);
-//     }
-//   }
-//   // Static function that makes MicroRegistry a class with single instance
-//   static MicroRegistry &getInstance() {
-//     static MicroRegistry instance;
-//     return instance;
-//   }
-//   // Register Micro
-//   bool RegisterMicro(const MicroAttrs &key, Micro *micro, std::string name) {
-//     ASSERT(micros.find(key) == micros.end());  // micro not registered
-//     micros.emplace(key, MicroRecord{micro, name});
-//     return true;
-//   }
-//   // Get micro by MicroAttrs {PlatformType, microType}
-//   Micro *getMicro(const MicroAttrs &microAttrs) const {
-//     auto it = micros.find(microAttrs);
-//     ASSERT(it != micros.end());
-//     return std::get<0>(it->second);
-//   }
-
-//   const MicroRecord &getMicroRecord(const MicroAttrs &microAttrs) {
-//     return micros.at(microAttrs);
-//   }
-// };
-
-// #define REGISTER_MICRO(platform, microType, micro, name)              \
-//   namespace infini {                                                  \
-//   static const bool _register_micro_##__COUNTER__ =                   \
-//       MicroRegistry::getInstance().registerMicro(                     \
-//           MicroAttrs{platform, microType, __COUNTER__}, micro, name); \
-//   }
 
 }  // namespace infini
