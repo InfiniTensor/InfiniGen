e1b44367ebba2bd1a51f500e7a1336cf883a3a39
Bolun <chamberlain0w0@gmail.com>
2023-09-25 09:16:52 +0000

    fix test_platform

--- test/test_platform.cpp
+++ test/test_platform.cpp
@@ -14,17 +14,17 @@ int main() {
   LOG(INFO) << cuda.taskIdx();
   LOG(INFO) << cuda.taskDim();
   LOG(INFO) << cuda.regDecl("float", "*cache");
-  LOG(INFO) << cuda.ldramDecl("half", "array_ldram");
-  LOG(INFO) << cuda.shmemDecl("int8", "arr");
-  LOG(INFO) << cuda.glmemDecl("double", "*a");
+  LOG(INFO) << cuda.ldramDecl("half", "*array_ldram");
+  LOG(INFO) << cuda.shmemDecl("int8", "*arr");
+  LOG(INFO) << cuda.glmemDecl("double", "a");
 
   LOG(INFO) << "Test BANG codegen";
-  LOG(INFO) << cuda.deviceFuncDecl("foo");
-  LOG(INFO) << cuda.globalFuncDecl("func");
-  LOG(INFO) << cuda.taskIdx();
-  LOG(INFO) << cuda.taskDim();
-  LOG(INFO) << cuda.regDecl("float", "*cache");
-  LOG(INFO) << cuda.ldramDecl("half", "array_ldram");
-  LOG(INFO) << cuda.shmemDecl("int8", "arr");
-  LOG(INFO) << cuda.glmemDecl("double", "*a");
+  LOG(INFO) << bang.deviceFuncDecl("foo");
+  LOG(INFO) << bang.globalFuncDecl("func");
+  LOG(INFO) << bang.taskIdx();
+  LOG(INFO) << bang.taskDim();
+  LOG(INFO) << bang.regDecl("float", "*cache");
+  LOG(INFO) << bang.ldramDecl("half", "*array_ldram");
+  LOG(INFO) << bang.shmemDecl("int8", "*arr");
+  LOG(INFO) << bang.glmemDecl("double", "a");
 }
