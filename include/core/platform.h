b07848310142eb3cb15e9b92349cf40c951305fc
zhangyue <138768300+zhangyue207@users.noreply.github.com>
2023-09-25 10:59:43 +0800

    add platform related handler (#26)
    * add platform related handler
    
    * format
    
    * update taskDecl
    
    * fix
    
    ---------
    
    Co-authored-by: wanghailu <wanghailu@qiyuanlab.com>

---
+++ include/core/platform.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "core/api.h"
+#include <string>
+#include <tuple>
+
+namespace infini {
+
+struct Platform {
+  using underlying_t = uint16_t;
+
+  enum : underlying_t { CUDA, BANG } type;
+
+  constexpr Platform(decltype(type) t) : type(t) {}
+  constexpr explicit Platform(underlying_t val)
+      : type((decltype(type))val) {}
+  constexpr underlying_t underlying() const { return type; }
+
+  bool operator==(Platform others) const { return type == others.type; }
+  bool operator!=(Platform others) const { return type != others.type; }
+
+  // Print platform related code
+  const std::string deviceFuncDecl() const;
+  const std::string globalFuncDecl() const;
+  const std::string taskIdxDecl(int dim) const;
+  const std::string taskIdxDecl() const;
+  const std::string taskDimDecl(int dim) const;
+  const std::string taskDimDecl() const;
+  const std::string regDecl() const;
+  const std::string ldramDecl() const;
+  const std::string shmemDecl() const;
+  const std::string glmemDecl() const;
+
+  const char *toString() const;
+  bool isCUDA() const;
+  bool isBANG() const;
+};
+
+}  // namespace infini
