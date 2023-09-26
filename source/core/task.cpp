df584725afffd13e48dafaa64c80e83f16559423
Bolun <chamberlain0w0@gmail.com>
2023-09-26 01:28:04 +0000

    fix details

--- source/core/task.cpp
+++ source/core/task.cpp
@@ -46,8 +46,9 @@ std::string SingleTask::generatorCode(Platform platform, int64_t indent = 0) {
     return "";
   }
   result += platform.deviceFuncDecl(name);
-  result += "(" + getArguments() + ") {\n" + indentation(indent + 1);
-
+  result += "(" + getArguments() + ") {\n";
+ 
+  result += indentation(indent + 1);
   result += "if (";
   for (int i = 0; i < core_list.size(); ++i) {
     result += platform.taskIdx() + " == " + std::to_string(core_list[i]);
@@ -65,6 +66,7 @@ std::string SingleTask::generatorCode(Platform platform, int64_t indent = 0) {
   for (int i = 0; i < micro_list.size(); ++i) {
     micro_list[i]->generatorCode(cache, result, indent + 2);
   }
+  
   result += indentation(indent + 1) + "}\n";
   result += indentation(indent) + "}\n";
   return result;
@@ -86,11 +88,11 @@ ParallelTask::ParallelTask(int64_t cache_length, int64_t swap_length,
 std::string ParallelTask::generatorCode(Platform platform, int64_t indent = 0) {
   std::string result = "\n" + indentation(indent);
   result += platform.deviceFuncDecl(name);
-  result += "(" + getArguments() + ") {\n" + indentation(indent + 1);
+  result += "(" + getArguments() + ") {\n";
 
   // TODO: delcare cache
-  result += platform.regDecl("char", cache.name) + "[" +
-            std::to_string(cache.cache_size) + "];\n";
+  result += indentation(indent + 1) + platform.regDecl("char", cache.name) +
+            "[" + std::to_string(cache.cache_size) + "];\n";
 
   result += indentation(indent + 1) +
             platform.ldramDecl("char", cache.name + "_ldram") + "[" +
