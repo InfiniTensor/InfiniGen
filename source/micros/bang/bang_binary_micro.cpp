631a58759a1b9ceda55298d7399e39ff258d30e1
Bolun Zhang <48948016+Chamberlain0w0@users.noreply.github.com>
2023-10-12 13:41:56 +0800

    Add binary (#42)
    * add bang binary micros
    
    * add cuda binary micros
    
    * add divbang
    
    * fix cuda equal
    
    * fix
    
    * fix divbang & cuda cast
    
    * add test

--- source/micros/bang/bang_binary_micro.cpp
+++ source/micros/bang/bang_binary_micro.cpp
@@ -29,6 +29,42 @@ namespace infini {
 BANG_GENERATOR(Add, "add")
 BANG_GENERATOR(Sub, "sub")
 BANG_GENERATOR(Mul, "mul")
+BANG_GENERATOR(Eq, "eq")
+BANG_GENERATOR(Ge, "ge")
+BANG_GENERATOR(Gt, "gt")
+BANG_GENERATOR(Le, "le")
+BANG_GENERATOR(Lt, "lt")
+BANG_GENERATOR(Ne, "ne")
+BANG_GENERATOR(And, "and")
+BANG_GENERATOR(Or, "or")
+BANG_GENERATOR(Xor, "xor")
+
+// Div
+std::string DivBang::generatorCode(Cache& cache, std::string& code,
+                                   int64_t indent) {
+  cache.lock();
+  std::string left_cache =
+      LoadBang(OperandType{left_name, left_offset, length, data_type})
+          .generatorCode(cache, code, indent);
+  std::string right_cache =
+      LoadBang(OperandType{right_name, right_offset, length, data_type})
+          .generatorCode(cache, code, indent);
+  std::string output_cache =
+      AllocateBang(OperandType{output_name, output_offset, length, data_type})
+          .generatorCode(cache, code, indent);
+  auto recip =
+      OperandType{right_name + "_recip", right_offset, length, data_type};
+  std::string recip_cache =
+      AllocateBang(recip).generatorCode(cache, code, indent);
+  code += indentation(indent) + "__bang_active_reciphp(" + recip_cache + ", " +
+          right_cache + ", " + std::to_string(length) + ");\n";
+  code += indentation(indent) + "__bang_mul(" + output_cache + ", " +
+          left_cache + ", " + recip_cache + ", " + std::to_string(length) +
+          ");\n";
+  cache.unlock();
+  FreeBang(recip).generatorCode(cache, code, indent);
+  return "";
+}
 
 /**
  * Register Micros
@@ -37,6 +73,16 @@ BANG_GENERATOR(Mul, "mul")
 REGISTER_MICRO(OperatorType::ADD, Platform::BANG, AddBang::makeObj)
 REGISTER_MICRO(OperatorType::SUB, Platform::BANG, SubBang::makeObj)
 REGISTER_MICRO(OperatorType::MUL, Platform::BANG, MulBang::makeObj)
+REGISTER_MICRO(OperatorType::DIV, Platform::BANG, DivBang::makeObj)
+REGISTER_MICRO(OperatorType::EQ, Platform::BANG, EqBang::makeObj)
+REGISTER_MICRO(OperatorType::GE, Platform::BANG, GeBang::makeObj)
+REGISTER_MICRO(OperatorType::GT, Platform::BANG, GtBang::makeObj)
+REGISTER_MICRO(OperatorType::LE, Platform::BANG, LeBang::makeObj)
+REGISTER_MICRO(OperatorType::LT, Platform::BANG, LtBang::makeObj)
+REGISTER_MICRO(OperatorType::NE, Platform::BANG, NeBang::makeObj)
+REGISTER_MICRO(OperatorType::AND, Platform::BANG, AndBang::makeObj)
+REGISTER_MICRO(OperatorType::OR, Platform::BANG, OrBang::makeObj)
+REGISTER_MICRO(OperatorType::XOR, Platform::BANG, XorBang::makeObj)
 
 #undef BANG_GENERATOR
 
